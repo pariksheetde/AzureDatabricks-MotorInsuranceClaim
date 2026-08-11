c43cd96bef964591edbffe1db8d1ea410a2fbec1
pde1409@hotmail.com <pde1409@hotmail.com>
2026-08-11 05:33:36 +0000

    COMMIT - SLOWLY CHANGING DIMENSION TYPE 2

--- MotorDLTPipeline/02.Motor-SparkDeclarativePipeline/SlowlyChangingDimension/02.OrdersStatus_SCD_II.py
+++ MotorDLTPipeline/02.Motor-SparkDeclarativePipeline/SlowlyChangingDimension/02.OrdersStatus_SCD_II.py
@@ -11,6 +11,6 @@ dp.create_auto_cdc_flow(name = 'ORDERS_STATUS_SCD_2',
                         apply_as_deletes = F.expr("_change_type = 'delete'"),
                         except_column_list = ['_change_type', '_commit_version', '_commit_timestamp'],
                         stored_as_scd_type = 2,
-                        track_history_columns = ['Status', 'TotalAmount']
+                        track_history_column_list = ['Status', 'TotalAmount']
 
 )
